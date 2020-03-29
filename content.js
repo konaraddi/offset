5640063ae61acbf7f8533267be50021df5bb0e4f
Omkar Konaraddi <okonarad@terpmail.umd.edu>
2020-03-29 17:43:00 -0400

    Add popup and use chrome.storage.sync to maintain total offset in extension

--- content.js
+++ content.js
@@ -1,24 +1,30 @@
-function saveData(
-  money,
-  tons,
-  callback = () => {
-    console.log(`update storage w/ ${money} and ${tons}`);
-  }
-) {
-  chrome.storage.sync.set({ money, tons }, callback);
-}
+let total = {
+    expenditure: 0.0,
+    carbon: 0.0
+  };
 
-function getData(
-  callback = ({ money, tons }) => {
-    console.log(`storage contains ${money} and ${tons}`);
+
+// reset
+// chrome.storage.sync.set(total, () => {console.log("RESET")})
+
+
+
+console.log(`CONTENT SCRIPT LOADED AT ${new Date().toLocaleTimeString()}`);
+
+
+chrome.storage.sync.get(
+  ["expenditure", "carbon"],
+  ({expenditure, carbon}) => {
+      console.log(`Savedtotal: \$${expenditure} ${carbon}`)
+    if (expenditure != null && carbon != null) {
+      total.expenditure = parseFloat(expenditure);
+      total.carbon = parseFloat(carbon);
+      console.log(`Fetched totals from storage: ${JSON.stringify(total)}`);
+    }
   }
-) {
-  chrome.storage.sync.get(["money", "tons"], data => callback(data));
-}
+);
 
 let showModal = () => {
-  console.log(`CONTENT SCRIPT LOADED AT ${new Date().toLocaleTimeString()}`);
-
   const url = location.href;
   const isJetcom = url == "https://jet.com/checkout";
   const isAmericanAirlines =
@@ -26,11 +32,9 @@ let showModal = () => {
     url.substring(0, 56) ==
       "https://www.aa.com/booking/passengers?bookingPathStateId";
 
-  /*
-    if (!isJetcom && !isAmericanAirlines) {
-      return;
-    }
-    */
+  if (!isJetcom && !isAmericanAirlines) {
+    return;
+  }
 
   document.body.innerHTML =
     `
@@ -52,32 +56,31 @@ let showModal = () => {
               
               <div class="level-item has-text-centered">
                 <div>
-                  <p id="counter2" class="title">$789</p>
+                  <p id="expense-counter" class="title">$789</p>
                   <p class="heading">donations (USD)</p>
                 </div>
               </div>
 
               <div class="level-item has-text-centered">
                 <div>
-                  <p id="counter1" class="title">3,456</p>
-                  <p class="heading">Tonns of Carbon</p>
+                  <p id="carbon-counter" class="title">3,456</p>
+                  <p class="heading">Tons of Carbon</p>
                 </div>
               </div>
-
             </div>
-            <h5 class="subtitle is-5">Offset 10 more tonnes of carbon to reach your goal!</h5>
+            <h5 class="subtitle is-5">Offset {{amountToSpend}} tons of carbon to reach your goal!</h5>
             <progress
               id="progress-bar"
               class="progress is-success is-medium"
               :value="progressBarValue"
-              max="100"
+              :max="goal"
             ></progress>
             <p>Your purchases are set to {{percent}}% of your purchases.</p>
             <p>You can change your percent, goal, and payment method in the extension's settings.</p>
           </section>
-          <footer class="modal-card-foot">
-            <button class="button is-danger">Cancel</button>
-            <button class="button">Learn more</button>
+          <footer class="modal-card-foot" style="marginTop: 0;">
+            <button class="button is-link">Okay</button>
+            <button class="button is-link is-light">Cancel</button>
           </footer>
         </div>
       </div>
@@ -89,30 +92,50 @@ let showModal = () => {
     el: "#app",
     data: {
       percent: 1,
-      goal: 100, 
+      goal: 5,
       isDonating: false,
       donated: false,
       isActive: true,
-      progressBarValue: 0,
+      progressBarValue: total.carbon
     },
     mounted() {
-      const countUp1 = new CountUp("counter1", 5234, { startVal: 2000 });
+    
+      const countUp1 = new CountUp(
+        "carbon-counter",
+        total.carbon + this.tons,
+        { startVal: total.carbon }
+      );
       if (!countUp1.error) {
         countUp1.start();
       } else {
-        console.error(countUp.error);
+        console.error(countUp1.error);
       }
 
-      const countUp2 = new CountUp("counter2", 7004, { startVal: 2000, prefix: "$" });
+      const countUp2 = new CountUp(
+        "expense-counter",
+        total.expenditure + this.money,
+        {
+          startVal: total.expenditure,
+          prefix: "$"
+        }
+      );
       if (!countUp2.error) {
         countUp2.start();
       } else {
-        console.error(countUp.error);
+        console.error(countUp2.error);
       }
 
-      setTimeout(() => this.progressBarValue = 40, 250)
+      
+      total.expenditure += parseFloat(this.money);
+      total.carbon += parseFloat(this.tons);
 
+
+      setTimeout(() => (this.progressBarValue = Number(total.carbon)), 250);
       setTimeout(() => (this.isActive = false), 6000);
+
+      chrome.storage.sync.set(total, () => {
+        console.log(`Saved ${JSON.stringify(total)} to storage`);
+      });
     },
     computed: {
       money() {
@@ -121,7 +144,7 @@ let showModal = () => {
             .getElementsByClassName(
               "base__BaseStyledComponent-sc-1l64hnd-0 typography__Text-sc-1lwzhqv-0 kvuhxc"
             )[12]
-            .innerHTML.substring(1);
+            .innerHTML.trim().substring(1);
           console.log(costOfOrder);
           return (0.01 * parseFloat(costOfOrder)).toFixed(2);
         }
@@ -137,6 +160,9 @@ let showModal = () => {
       },
       tons() {
         return (this.money / 3.3).toFixed(2);
+      },
+      amountToSpend() {
+        return (Number(this.goal) - Number(total.carbon)).toFixed(2)
       }
     },
     methods: {
